572eb0704555f4edd2c2fdaf3f0e0fe015646df6
Maximilien Chaumon <maximilien.chaumon@icm-institute.org>
2023-02-27 13:23:46 +0100

    write events.tsv directly, instead of relying on write_raw_bids

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import numpy as np
 import mne
 import json
 import helper
-from mne_bids import BIDSPath,write_raw_bids
+from mne_bids.write import _events_tsv
 import shutil
 import matplotlib.pyplot as plt
 
@@ -17,9 +17,6 @@ with open('config.json') as config_json:
 
 data_file = config['fif']
 raw = mne.io.read_raw_fif(data_file,verbose=False)
-
-
-
 
 events = mne.find_events(raw,stim_channel=config['stim_channel'],
                             output=config['output'],
@@ -39,33 +36,8 @@ report.add_events(events=events, title='Events',sfreq=raw.info['sfreq'])
 
 fig = mne.viz.plot_events(events, sfreq=raw.info['sfreq'],
                           first_samp=raw.first_samp)
-
-# Create a BIDSPath
-bids_path = BIDSPath(subject='subject',
-                     session=None,
-                     task='task',
-                     run='01',
-                     acquisition=None,
-                     processing=None,
-                     recording=None,
-                     space=None,
-                     suffix=None,
-                     datatype='meg',
-                     root='bids')
-
-event_id = {}
-for ev in events[:,2]:
-    event_id[str(ev)] = ev
     
-# # Write BIDS to create events.tsv BIDS compliant
-write_raw_bids(raw, bids_path, events_data=events, event_id=event_id, overwrite=True)
-#
-# # Extract events.tsv from bids path
-events_file = 'bids/sub-subject/meg/sub-subject_task-task_run-01_events.tsv'
-#
-# # Copy events.tsv in outdir
-shutil.copy2(events_file, 'out_dir/events.tsv')
-
+_events_tsv(events, np.repeat(0.,events.shape[0]), raw, 'out_dir/events.tsv', trial_type=None, overwrite=True)
 
 # == SAVE REPORT ==
 report.save('out_dir_report/report.html', overwrite=True)
