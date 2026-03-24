034bd979bc2850147180a866d85384765bfba543
Maximilien Chaumon <maximilien.chaumon@protonmail.com>
2026-03-24 14:06:53 +0100

    refactor: use shared brainlife_utils library

--- main.py
+++ main.py
@@ -1,47 +1,72 @@
-import os
-import numpy as np
-import mne
-import json
-import helper
-from mne_bids.write import _events_tsv
-import shutil
-import matplotlib.pyplot as plt
+"""Brainlife App for MNE-Python event detection.
 
-#workaround for -- _tkinter.TclError: invalid command name ".!canvas"
+This App loads a `.fif` file, finds events, and saves them to a `.tsv` file.
+It also generates a report with a plot of the events.
+"""
+
+# Copyright (c) 2026 brainlife.io
+#
+# Authors:
+# - Maximilien Chaumon (https://github.com/dnacombo)
+# - Kamilya Salibayeva (https://github.com/KSalibay)
+# - Guiomar Niso (https://github.com/guiomar)
+
+import mne
+import numpy as np
+from mne_bids.write import _events_tsv
+import sys
+import os
+
+# a workaround for matplotlib TclError
 import matplotlib
 matplotlib.use('Agg')
 import matplotlib.pyplot as plt
 
-with open('config.json') as config_json:
-    config = helper.convert_parameters_to_None(json.load(config_json))
 
-data_file = config['mne']
-raw = mne.io.read_raw_fif(data_file,verbose=False)
+# import brainlife_utils
+sys.path.insert(0, os.path.join(os.path.dirname(__file__), 'brainlife_utils'))
+from brainlife_utils import (
+    load_config,
+    setup_matplotlib_backend,
+    ensure_output_dirs,
+    create_product_json,
+    add_image_to_product,
+    add_info_to_product
+)
+from brainlife_utils.report import make_report_events
 
 
-events = mne.find_events(raw)
-'''
-events = mne.find_events(raw,stim_channel=config['stim_channel'],
-                            output=config['output'],
-                            consecutive=config['consecutive'],
-                            min_duration=config['min_duration'],
-                            shortest_event=config['shortest_event'],
-                            mask=config['mask'],
-                            uint_cast=config['uint_cast'],
-                            mask_type=config['mask_type'],
-                            initial_event=config['initial_event'])
-'''
+# set up environment
+setup_matplotlib_backend()
+config = load_config()
+ensure_output_dirs('out_dir', 'out_figs', 'out_report')
 
-events = mne.pick_events(events,include=config['include'],exclude=config['exclude'])
 
-report = mne.Report(title='Events')
+# load data
+data_file = config.pop('mne')
+raw = mne.io.read_raw_fif(data_file, verbose=False)
 
-report.add_events(events=events, title='Events',sfreq=raw.info['sfreq'])
 
+# find events
+events = mne.find_events(raw, **config)
+
+
+# save events to tsv
+_events_tsv(events, np.repeat(0., events.shape[0]), raw, 'out_dir/events.tsv',
+            trial_type=None, overwrite=True)
+
+# generate report
+report = make_report_events(events, raw.info)
+
+
+# save report
+report.save('out_report/report.html', overwrite=True)
+
+# create product.json
+product_items = []
 fig = mne.viz.plot_events(events, sfreq=raw.info['sfreq'],
                           first_samp=raw.first_samp)
-    
-_events_tsv(events, np.repeat(0.,events.shape[0]), raw, 'out_dir/events.tsv', trial_type=None, overwrite=True)
-
-# == SAVE REPORT ==
-report.save('out_dir_report/report.html', overwrite=True)
+fig_path = "out_figs/events.png"
+fig.savefig(fig_path)
+add_image_to_product(product_items, "Events", fig_path)
+create_product_json(product_items)
